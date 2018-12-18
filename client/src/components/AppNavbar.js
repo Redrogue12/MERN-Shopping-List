0868c0eb617b16b7e24c5eda24d4de31ea711efd
Redrogue12 <exgc12@gmail.com>
2018-12-18 15:15:06 -0400

    Navigation Bar

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
+class AppNavBar extends Component {
+  
+    state = {
+      isOpen: false
+    }
+
+    toggle = () => {
+      this.setState({
+        isOpen: !this.state.isOpen
+      })
+    }
+
+    render() {
+      return (
+        <div>
+          <Navbar color="dark" dark expand="sm" className="mb-5">
+            <Container>
+              <NavbarBrand href="/">ShoppingList</NavbarBrand>
+              <NavbarToggler onClick={this.toggle}></NavbarToggler>
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="http://google.com">Google</NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Container>
+          </Navbar>
+        </div>
+      )
+    }
+  
+}
+
+export default AppNavBar;
