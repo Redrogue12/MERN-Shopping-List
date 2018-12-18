2363a2706d5de859e8ea3d0fbdafc31b73df407f
Redrogue12 <exgc12@gmail.com>
2018-12-18 15:15:00 -0400

    Added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import AppNavBar from "./components/AppNavbar";
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hello world!</h1>
+        <AppNavBar></AppNavBar>
       </div>
     );
   }
