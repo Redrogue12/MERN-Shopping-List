35ad0a18b9ac765aa03684b6400e7a2ae816de45
Redrogue12 <exgc12@gmail.com>
2019-01-17 22:27:01 -0400

    Removed unused elements.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import {
-  Container
-} from 'reactstrap';
 
 class Footer extends Component {
   render() {
     return (
       <footer className="footer">
         <div className="container">
-          <span className="text-muted">By: Edgar X. González Cortés</span>
+          By: Edgar X. González Cortés
         </div>
       </footer>
     )
